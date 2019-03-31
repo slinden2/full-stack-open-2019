ecca771a49ea0e779281c827d196be984669dfd7
Samu <samu.linden@gmail.com>
2019-03-31 17:11:51 +0200

    Osa2a tehtävät korjaus2

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,52 +1,8 @@
 import React from 'react'
 import Courses from './components/Courses'
 
-const App = () => {
-    const courses = [
-      {
-        name: 'Half Stack -sovelluskehitys',
-        id: 1,
-        parts: [
-          {
-            name: 'Reactin perusteet',
-            exercises: 10,
-            id: 1
-          },
-          {
-            name: 'Tiedonvälitys propseilla',
-            exercises: 7,
-            id: 2
-          },
-          {
-            name: 'Komponenttien tila',
-            exercises: 14,
-            id: 3
-          },
-          {
-            name: 'Redux',
-            exercises: 7,
-            id: 4
-          }
-        ]
-      },
-      {
-        name: 'Node.js',
-        id: 2,
-        parts: [
-          {
-            name: 'Routing',
-            exercises: 3,
-            id: 1
-          },
-          {
-            name: 'Middlewaret',
-            exercises: 7,
-            id: 2
-          }
-        ]
-      }
-    ]
-  
+const App = ({ courses }) => {
+    
     return (
       <>
         <h1>Opetusohjelma</h1>
