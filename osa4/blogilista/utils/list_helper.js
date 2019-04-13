0d68f7962634f122f093ba8aa857fa74b122eb03
Samu <samu.linden@gmail.com>
2019-04-13 20:58:00 +0200

    Tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) =>
+    ({ likes: total.likes + blog.likes })).likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
