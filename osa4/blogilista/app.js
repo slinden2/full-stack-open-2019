ccaf1768c28aba062d2a774abd4317e8b14673d1
Samu <samu.linden@gmail.com>
2019-04-13 18:28:11 +0200

    Tehtävä 4.2 controllers-kansio luotu

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,35 +3,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Blog = require('./models/blog')
 const mongoose = require('mongoose')
+const blogsRuoter = require('./controllers/blogs')
+
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(blog => blog.toJSON()))
-    })
-    .catch(error => next(error))
-})
-
-app.post('/api/blogs', (request, response, next) => {
-
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
-})
+app.use('/api/blogs', blogsRuoter)
 
 const unknownEndpoint = (request, response) => {
   return response.status(404).json({ error: 'unknown endpoint' })
