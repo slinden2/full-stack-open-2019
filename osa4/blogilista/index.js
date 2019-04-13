f44a54940a6599d288fdbc32447a99086424da11
Samu <samu.linden@gmail.com>
2019-04-13 18:20:04 +0200

    Tehtävä 4.2 app.js luotu

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,55 +1,9 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const Blog = require('./models/blog')
-const mongoose = require('mongoose')
+const http = require('http')
+const app = require('./app')
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(blog => blog.toJSON()))
-    })
-    .catch(error => next(error))
-})
-
-app.post('/api/blogs', (request, response, next) => {
-
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
-})
+const server = http.createServer(app)
 
 const PORT = 3003
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-const unknownEndpoint = (request, response) => {
-  return response.status(404).json({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
