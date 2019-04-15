e8bfb6de7b4936e60489d2302f90941a24bc304a
Samu <samu.linden@gmail.com>
2019-04-15 15:42:38 +0200

    Tehtävä 4.8 refaktorointi

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,13 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response, next) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs.map(blog => blog.toJSON()))
-    })
-    .catch(error => next(error))
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog => blog.toJSON()))
+  } catch (exception) {
+    next(exception)
+  }
 })
 
 blogsRouter.post('/', (request, response, next) => {
