6f73278a66443bfb76a0b3547d618b3088dbd035
Samu <samu.linden@gmail.com>
2019-04-19 18:30:35 +0200

    Tehtävä 4.15

---
+++ osa4/blogilista/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+})
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
