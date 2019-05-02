bc90b134d60a2041981dd7517ef855a09c7ea857
Samu <samu.linden@gmail.com>
2019-05-02 20:44:14 +0200

    Pikkukorjaus

--- osa5/bloglist/frontend/src/components/Blog.js
+++ osa5/bloglist/frontend/src/components/Blog.js
@@ -4,7 +4,6 @@ import blogService from '../services/blogs'
 
 const Blog = ({ user, blog, blogs, setBlogs, notify }) => {
   const [visible, setVisible] = useState(false)
-  const [likes, setLikes] = useState(blog.likes)
 
   let showRemove = false
   if (user.username === blog.user.username) showRemove = true
@@ -22,10 +21,9 @@ const Blog = ({ user, blog, blogs, setBlogs, notify }) => {
   }
 
   const updateLikes = async () => {
-    const newBlog = { ...blog, likes: likes + 1 }
+    const newBlog = { ...blog, likes: blog.likes + 1 }
     await blogService.update(newBlog)
     setBlogs(blogs.filter(b => blog.id !== b.id).concat(newBlog))
-    setLikes(likes + 1)
   }
 
   const removeBlog = async () => {
@@ -45,7 +43,7 @@ const Blog = ({ user, blog, blogs, setBlogs, notify }) => {
       </div>
       <div style={toggle(visible)}>
         {blog.url} <br />
-        {likes}
+        {blog.likes}
         <button onClick={updateLikes}>like</button> <br />
         added by {blog.user.name} <br />
         <button style={toggle(showRemove)} onClick={removeBlog}>remove</button>
