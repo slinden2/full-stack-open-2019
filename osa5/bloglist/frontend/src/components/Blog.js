4db431433a01dcc43bbd39fe8339cdf9e24cb44b
Samu <samu.linden@gmail.com>
2019-05-02 12:30:55 +0200

    Tehtävä 5.6

--- osa5/bloglist/frontend/src/components/Blog.js
+++ osa5/bloglist/frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetails = { display: visible ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setVisible(!visible)}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={toggleDetails}>
+        {blog.url} <br />
+        {blog.likes}
+        <button>like</button> <br />
+        added by {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
