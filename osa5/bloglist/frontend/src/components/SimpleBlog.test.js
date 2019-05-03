61fc1e9179ab17cbfdbad63f911f6fb6822d951c
Samu <samu.linden@gmail.com>
2019-05-03 15:24:52 +0200

    Tehtävä 5.14

--- osa5/bloglist/frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist/frontend/src/components/SimpleBlog.test.js
@@ -1,30 +1,43 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 describe('<SimpleBlog />', () => {
 
-  it('renders title and likes', () => {
+  const blog = {
+    title: 'Blogi 1',
+    author: 'Blogaaja 1',
+    url: 'http://www.blogi.com',
+    likes: 5
+  }
 
-    const blog = {
-      title: 'Blogi 1',
-      author: 'Blogaaja 1',
-      url: 'http://www.blogi.com',
-      likes: 5
-    }
+  let component
+  let likeCallBack
 
-    const component = render(
+  beforeEach(() => {
+    likeCallBack = jest.fn()
+
+    component = render(
       <SimpleBlog
         blog={blog}
-        onClick={() => 'handler called'}
+        onClick={likeCallBack}
       />
     )
+  })
 
+  it('renders title and likes', () => {
     const titleElement = component.container.querySelector('.title')
-
     expect(titleElement).toHaveTextContent('Blogi 1')
     expect(titleElement).toHaveTextContent('Blogaaja 1')
     expect(component.container.querySelector('.likes')).toHaveTextContent(/blog has \d+ likes/)
+  })
+
+  it('like button registers two clicks correctly', () => {
+    const button = component.container.querySelector('button')
+    fireEvent.click(button)
+    expect(likeCallBack.mock.calls.length).toBe(1)
+    fireEvent.click(button)
+    expect(likeCallBack.mock.calls.length).toBe(2)
   })
 
 })
