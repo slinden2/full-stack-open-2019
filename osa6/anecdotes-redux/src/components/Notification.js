bc71083fadf195c2e696ca8f50fd36ef6fc40d07
Samu <samu.linden@gmail.com>
2019-05-06 20:16:01 +0200

    6.10 korjaus

--- osa6/anecdotes-redux/src/components/Notification.js
+++ osa6/anecdotes-redux/src/components/Notification.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 const Notification = ({ store }) => {
+  const notification = store.getState().notification
+
   const style = {
     border: 'solid',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 1,
+    display: notification.message ? '' : 'none'
   }
 
   return (
