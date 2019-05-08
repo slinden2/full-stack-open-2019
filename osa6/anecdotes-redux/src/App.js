32d21e2571041e3614f03df7ca9589dd2442470b
Samu <samu.linden@gmail.com>
2019-05-08 21:23:26 +0200

    Tehtävä 6.16

--- osa6/anecdotes-redux/src/App.js
+++ osa6/anecdotes-redux/src/App.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import { createNotification, hideNotification } from './reducers/notificationReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteService from './services/anecdotes'
 
 const App = props => {
   const [timeoutId, setTimeoutId] = useState(0)
@@ -16,6 +18,12 @@ const App = props => {
       props.hideNotification()
     }, 5000))
   }
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then(anecdotes => props.initializeAnecdotes(anecdotes))
+  }, [])
 
   return (
     <div>
@@ -32,7 +40,8 @@ const App = props => {
 
 const mapDispatchToProps = {
   createNotification,
-  hideNotification
+  hideNotification,
+  initializeAnecdotes
 }
 
 const ConnectedApp = connect(null, mapDispatchToProps)(App)
