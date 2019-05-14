fef325ea5a189894c2e0186e9075213b83b8d0d1
Samu <samu.linden@gmail.com>
2019-05-14 11:01:05 +0200

    Tehtävä 7.8

--- osa7/bloglist/frontend/src/components/Users.js
+++ osa7/bloglist/frontend/src/components/Users.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Users = props => {
 
