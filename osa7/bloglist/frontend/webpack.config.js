7c9773a121960f24ca281de6d8eb6ce7f28704c8
Samu <samu.linden@gmail.com>
2019-05-15 15:29:14 +0200

    Commit ennen ejectia

---
+++ osa7/bloglist/frontend/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+const config = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader, css-loader']
+      }
+    ]
+  }
+}
+
+module.exports = config
