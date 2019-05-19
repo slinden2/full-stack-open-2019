853ed28784f8e0b502bed54b27ebc5262780817f
Samu <samu.linden@gmail.com>
2019-05-19 18:47:19 +0200

    Tehtävä 8.6

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -1,4 +1,5 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const uuid = require('uuid/v1')
 
 let authors = [
   {
@@ -103,6 +104,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -121,6 +131,25 @@ const resolvers = {
   Author: {
     bookCount: root => (books.filter(b => b.author === root.name)).length
   },
+  Mutation: {
+    addBook: (root, args) => {
+      console.log(root);
+      console.log(args);
+      if (books.find(b => b.title === args.title)) {
+        throw new UserInputError('title must be unique', {
+          invalidArgs: args.title
+        })
+      }
+
+      if (!authors.includes(args.author)) {
+        authors = authors.concat({ name: args.author, id: uuid() })
+      }
+
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
+  }
 }
 
 const server = new ApolloServer({
