e97011577461d70963bbee46d7ffa612621ed0d5
Samu <samu.linden@gmail.com>
2019-05-20 14:12:52 +0200

    Tehtävä 8.8

---
+++ osa8/library/frontend/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from 'react-apollo-hooks'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const ALL_AUTHORS = gql`
+  {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const result = useQuery(ALL_AUTHORS)
+  console.log(result)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+        result={result}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
